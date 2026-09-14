797acc5f56ff2d20967aafdf929245d494c93514
inv-abidmuhammad <abid.muhammad@innovaturelabs.com>
2026-09-14 18:07:56 +0530

    feat: add Dockerfile, requirements.txt, and .dockerignore for project setup

--- async-document-processor/config/settings.py
+++ async-document-processor/config/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv("DEBUG", "False") == "True"
